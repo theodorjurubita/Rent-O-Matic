b1bca24fbb0cb76484801d49f48523d7c46da0ab
jupyt <jupyt@DESKTOP-FCQORBA>
2017-03-06 19:23:57 +0200

    Add project files.

---
+++ Rent-O-Matic/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rent_O_Matic.MvcApplication" Language="C#" %>
---
+++ Rent-O-Matic/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rent_O_Matic.Startup))]
+namespace Rent_O_Matic
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
