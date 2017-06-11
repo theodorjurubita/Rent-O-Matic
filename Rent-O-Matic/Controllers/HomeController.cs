d135c164ca8a53c2cfa0321d4315455afbfb9604
Theodor <theodor.jurubita@gmail.com>
2017-06-11 18:52:35 +0300

    Added HomePage for displaying the rented cars for customer

--- Rent-O-Matic/Controllers/HomeController.cs
+++ Rent-O-Matic/Controllers/HomeController.cs
@@ -1,13 +1,56 @@
-﻿using System.Web.Mvc;
+﻿using Microsoft.AspNet.Identity;
+using Rent_O_Matic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Rent_O_Matic.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+
+        }
         public ActionResult Index()
         {
-            return View();
+            try
+            {
+                var userId = User.Identity.GetUserId();
+                var customerAssociatedWithUser = _context.Customers.Single(c => c.UserId == userId);
+                var carEntriesFromHistory = _context.RentalsHistories
+                    .Where(h => h.CustomerId == customerAssociatedWithUser.Id)
+                    .Where(h => h.DateRented <= DateTime.Today && h.DateReturned >= DateTime.Today)
+                    .ToList();
+
+                var carsRented = new List<RentedCarsViewModel>();
+
+                foreach (var carEntry in carEntriesFromHistory)
+                {
+                    var car = _context.Cars.Single(c => c.Id == carEntry.CarId);
+                    var store = _context.Stores.Single(s => s.Id == car.StoreId);
+                    var carRented = new RentedCarsViewModel()
+                    {
+                        Brand = car.Brand,
+                        Model = car.Model,
+                        PricePerDay = car.Price,
+                        Store = store.City + ", " + store.Country,
+                        DateRented = carEntry.DateRented,
+                        DateReturned = carEntry.DateReturned
+                    };
+                    carsRented.Add(carRented);
+                }
+                return View(carsRented);
+            }
+            catch
+            {
+                return View();
+            }
         }
 
         public ActionResult About()
---
+++ Rent-O-Matic/ViewModels/RentedCarsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Rent_O_Matic.ViewModels
+{
+    public class RentedCarsViewModel
+    {
+        public DateTime DateRented { get; set; }
+
+        public DateTime DateReturned { get; set; }
+
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string Store { get; set; }
+
+        public float PricePerDay { get; set; }
+    }
+}
